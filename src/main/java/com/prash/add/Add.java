8e66227313c1a2df86a7dc6f5b1df6e2e8edeab3
spprash <prashanth.premkumar@cognizant.com>
2018-04-26 21:21:32 -0400

    dev

--- src/main/java/com/prash/add/Add.java
+++ src/main/java/com/prash/add/Add.java
@@ -7,11 +7,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
+
 public class Add {
 
-	@RequestMapping("/add")
-	String add() {
-		return "ADD";
+	@RequestMapping(value="/add/API")
+	@ResponseBody
+	String addAPI() {
+		return "API is /add/{N1}/{N2} or /add?param1={N1}&param2={N2}";
+	}
+
+	@RequestMapping(value="/add/{param1}/{param2}")
+	@ResponseBody
+	int addPathStyle(@PathVariable int param1, @PathVariable int param2) {
+		return param1+param2;
+	}
+
+	@RequestMapping(value="/add")
+	@ResponseBody
+	int addParamStyle(@RequestParam("param1") int param1, @RequestParam("param2") int param2) {
+		return param1+param2;
+	}
+
+	@RequestMapping(value="/add/JSON",method = RequestMethod.POST)
+	@ResponseBody
+	AddDO addJSONStyle(@RequestBody AddDO addDO) {
+		addDO.result = addDO.param1+addDO.param2;
+		return addDO;
 	}
 
 	public static void main(String[] args) throws Exception {
--- src/test/java/com/prash/add/AddTest.java
+++ src/test/java/com/prash/add/AddTest.java
@@ -1,9 +1,9 @@
 package com.prash.add;
 
 import org.junit.Test;
-import org.junit.runner.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,10 +20,36 @@ public class AddTest {
     private MockMvc mvc;
  
     @Test
-    public void testAdd() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/add").accept(MediaType.APPLICATION_JSON))
+    public void testAddOk() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/add/API").accept(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.content().string("ADD"));
+        .andExpect(MockMvcResultMatchers.content().string("API is /add/{N1}/{N2} or /add?param1={N1}&param2={N2}"));
+    }
+
+    @Test
+    public void testAddPathStyle() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/add/1/2").accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("3"));
+    }
+
+
+    @Test
+    public void testAddParamStyle() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/add?param1=1&param2=2").accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("3"));
+    }
+
+    @Test
+    public void testAddJSONStyle() throws Exception {
+        String requestJson = "{\"param1\":1,\"param2\":2,\"result\":0}";
+        String responseJson = "{\"param1\":1,\"param2\":2,\"result\":3}";
+
+        mvc.perform(MockMvcRequestBuilders.post("/add/JSON").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+        .content(requestJson))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().json(responseJson));
     }
 
 }
